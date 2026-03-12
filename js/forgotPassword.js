fe613ee66c1f7c9b97b8a8d6aa5a1eab0af82048
EmmanuelPerezDEV <emmanperez.19@gmail.com>
2026-03-11 19:12:53 -0600

    Se enlazo el frontend de contraseña olvidada en la ventana de inisiar sesion

--- js/forgotPassword.js
+++ js/forgotPassword.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <div class="success-icon">✓</div>
         <h1 class="login-titulo">Revisa tu correo</h1>
         <p class="login-subtitulo mensaje-final">
-          Te enviaremos instrucciones para restablecer tu contraseña si el correo que ingresaste coincide con una cuenta registrada.
+          Si el correo que ingresaste coincide con una cuenta registrada te enviaremos instrucciones para restablecer tu contraseña. 
         </p>
         <a href="index.html" class="btn-home">Ir al inicio</a>
       </div>
